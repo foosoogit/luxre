ea0aa8051f39c96dcce7c9dc786fde034929a912
foosoogit <awa@szemi-gp.com>
2024-01-16 10:43:30 +0900

    戻る処理4

--- project/resources/views/livewire/daily-report.blade.php
+++ project/resources/views/livewire/daily-report.blade.php
@@ -7,20 +7,21 @@
                     <div class="col-auto">
                         <a class="btn mb-2 btn-primary" href="{{route('admin.top')}}">メニューに戻る</a>
                     </div>
-                    
-                    <div class="col-auto">
-                        <form method="POST" action="{{route($target_historyBack_inf_array[1])}}">@csrf
-                            @if(isset( $target_day ))
-                                <input name="target_day" type="hidden" value="{{$target_day}}"/>
-                                <button type="submit" name="target_date" class="btn btn-primary" value="{{$target_day}}">{{$target_historyBack_inf_array[0]}}に戻る</button>
-                            @endif
-                            @if(isset( $today ))
-                                <input name="year_month_day" type="hidden" value="{{$today}}"/>
-                                <button type="submit" name="target_date" class="btn btn-primary" value="{{$today}}">{{$target_historyBack_inf_array[0]}}に戻る</button>
-                            @endif
-                            <input name="back_flg" type="hidden" value="true"/>
-                        </form>
-                    </div>
+                    @if($target_historyBack_inf_array[1]!=='admin.top')
+                        <div class="col-auto">
+                            <form method="POST" action="{{route($target_historyBack_inf_array[1])}}">@csrf
+                                @if(isset( $target_day ))
+                                    <input name="target_day" type="hidden" value="{{$target_day}}"/>
+                                    <button type="submit" name="target_date" class="btn btn-primary" value="{{$target_day}}">{{$target_historyBack_inf_array[0]}}に戻る</button>
+                                @endif
+                                @if(isset( $today ))
+                                    <input name="year_month_day" type="hidden" value="{{$today}}"/>
+                                    <button type="submit" name="target_date" class="btn btn-primary" value="{{$today}}">{{$target_historyBack_inf_array[0]}}に戻る</button>
+                                @endif
+                                <input name="back_flg" type="hidden" value="true"/>
+                            </form>
+                        </div>
+                    @endif
                     {{--
                     @if($from_place=="monthly_rep")
                         <div class="col-auto">
